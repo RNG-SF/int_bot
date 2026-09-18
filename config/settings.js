e67c3b785c1d4c405157d9ab2503afad3cb936fc
RNG_SF <sofyantorangga028@gmail.com>
2026-09-18 19:37:20 +0700

    Delete config directory

--- config/settings.js
+++
@@ -1,2 +0,0 @@
-const RNG_SETTINGS={appName:"RNG_NET",version:"Stable Modular",animation:{enabled:true,entrance:true,accessReveal:true,stagger:true},access:{showNameBeforeMessage:true,linkRequiresButton:true,supportPassword4:true}};
-const SETTINGS=RNG_SETTINGS;
